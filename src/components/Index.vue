0138e2279c7e9b3a225837983fcb7a33bb4ba783
uames <uames.ji@gmail.com>
2018-11-27 10:03:41 +0800

    init-add-cubeui

---
+++ src/components/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="menus">
+    <div class="menu" v-for="(menu,idx) in menus" :key="idx" :style="{background:menu.color}">
+      {{ menu.text }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  props: {
+    menus: Array
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.menus{
+  display: flex;
+  flex-flow: row wrap;
+  color:#fff;
+  font-size: .5rem;
+  .menu{
+    flex: 0 0 44%;
+    margin:.2rem 3%;
+    height: 1rem;
+    line-height: 1rem;
+    border-radius: .1rem;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <Index :menus="menus" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Index from "@/components/Index.vue";
+
+export default {
+  name: "home",
+  components: {
+    Index
+  },
+  data() {
+    return {
+      menus: [
+        { text: "商品订购", color: "#00f"},{ text: "主商品变更", color: "#f00" },
+        { text: "订购商品查询", color: "#0f0" },{ text: "客户资料查询", color: "#0ff" }
+      ]
+    };
+  }
+};
+</script>
