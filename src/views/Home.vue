0e2c3379e621f7db69780b5cad8777d456c44557
zihong.tang <245500200@qq.com>
2019-03-27 15:05:36 +0800

    页面更新2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,7 +20,12 @@ export default {
         {text:"商品订购",color:"#c4f",link:'#/goodsorder'},
         {text:"主商品变更",color:"#f00",link:'#/mainchange'},
         {text:"订购商品查询",color:"#0f0",link:'#/'},
-        {text:"客户资料查询",color:"#0ff",link:'#/'}
+        {text:"客户资料查询",color:"#0ff",link:'#/'},
+        {text:"日志查询",color:"#0ff",link:'#/'},
+        {text:"11111",color:"#0ff",link:'#/'},
+        {text:"2222",color:"#0ff",link:'#/'},
+        {text:"3333",color:"#0ff",link:'#/'},
+        {text:"4444",color:"#0ff",link:'#/'}
       ]
     };
   }
